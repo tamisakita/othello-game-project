f469e5702ad0bfa306ce89af61597f9656dcdac6
Tami Pipoca <tamisakita@hotmail.com>
2020-08-10 22:35:14 -0300

    fixed the css and add some js

---
+++ index.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line max-len
+// criar funcao para verificar se posso posicionar a peça naquele square (caso nao tenha como posicionar, passar a vez para o outro player)
+
+// caso seja valido, a peça aparece se eu clicar no square
+
+// criar funcao para as peças do oponente virar quando eu posicionar uma peça
+
+// contar a quantidade de peça de cada player e verificar quem tem mais peças, se for igual é tie
+
+// como faço pra mudar as cores da peça?
+
+let player1 = 'black';
+let player2 = 'white';
+
+function validPosition() {
+
+}
+
+function flipOponentPiece() {
+
+}
+
+function gameOver() {
+
+}
